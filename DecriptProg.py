20ce20e1a3af6a42f94fa83e057d6a9b85d83deb
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-09-06 19:51:40 -0600

    Change signature of mp4

--- DecriptProg.py
+++ DecriptProg.py
@@ -5,9 +5,9 @@ def decode_mod(data):
                         'png': [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a], 
                         'pdf': [0x25, 0x50, 0x44, 0x46, 0x2D], 
                         'mp3': [0x49, 0x44, 0x33], 
-                        'mp4': [0x20, 0x66, 0x74, 0x79, 0x70]
+                        'mp4': [0x66, 0x74, 0x79, 0x70]
                     }
-    file = open("file2.lol", "rb")
+    file = open("file4.lol", "rb")
     data = file.read()
     numbers = [x for x in data] # bytes as integers
     hexadecimal = [hex(x) for x in numbers ] # bytes as hexadecimal
@@ -21,9 +21,11 @@ def decode_mod(data):
     for key in file_signatures1:
         decimal_signatures[key] = [int(x) for x in file_signatures1[key]]
 
-
+    BLUE = '\033[94m'
+    ENDC = '\033[0m'
     for key in decimal_signatures:
         #Make the ecuation system to find alpha and beta
+        print(BLUE, "KEY: ", key, ENDC)
         if key == 'mp4': #If its an mp4, we need to start from the fifth byte
             first_file = decimal_file[4] # First byte of the file
             second_file = decimal_file[5] # Second byte of the file
@@ -42,7 +44,7 @@ def decode_mod(data):
         print("Tryingf:", alpha_mult, "a = " , right_side, "mod 256")
 
         inverses_alpha_mult = [x for x in range(256) if (x * alpha_mult) % 256 == 1] # Get the inverses of alpha_mult
-        print("Inverses: ", inverses_alpha_mult)
+        #print("Inverses: ", inverses_alpha_mult)
 
         if len(inverses_alpha_mult) == 0: # If we are not working with something that has inverse
 
@@ -51,9 +53,9 @@ def decode_mod(data):
 
             inverses_alpha_mult = [x for x in range(256) if (x * alpha_mult) % 256 == 1] # Get the inverses of alpha_mult
 
-            print("Trying: (", first_sign," - ", second_sign, ") a = ", first_file, " - ", second_file, " = " , right_side, "mod 256")
-            print("Trying: ", alpha_mult, "a = " , right_side, "mod 256")
-            print("Inverses: ", inverses_alpha_mult)
+            #print("Trying: (", first_sign," - ", second_sign, ") a = ", first_file, " - ", second_file, " = " , right_side, "mod 256")
+            #print("Trying: ", alpha_mult, "a = " , right_side, "mod 256")
+            #print("Inverses: ", inverses_alpha_mult)
 
             if inverses_alpha_mult == []:
                 print("No tiene inverso")
@@ -61,20 +63,22 @@ def decode_mod(data):
             else:
                 alpha = right_side * inverses_alpha_mult[0] % 256
                 beta = (first_file - (first_sign * alpha)) % 256
-                print("alpha: ", alpha)
-                print("beta: ", beta)
+                #print("alpha: ", alpha)
+                #print("beta: ", beta)
 
         else:
             alpha = right_side * inverses_alpha_mult[0] % 256
             beta = (first_sign - (first_file * alpha)) % 256
-            print("alpha: ", alpha)
-            print("beta: ", beta)
+            #print("alpha: ", alpha)
+            #print("beta: ", beta)
 
 
         # Apply the operations to each byte of the file
         decoded = []
         for i,num in enumerate(decimal_file):
             decoded.append((num * alpha + beta) % 256)
+
+        print("Decoded: ", decoded[:10])
 
         # Check if the fist bytes are the same as the signature
         signature = file_signatures1[key]
@@ -88,7 +92,7 @@ def decode_mod(data):
 
         # Write the file
         if found:
-            file = open("file2_decrypted." + key, "wb")
+            file = open("file4_decrypted." + key, "wb")
             file.write(bytearray(decoded))
             file.close()
             break
@@ -97,7 +101,7 @@ def decode_mod(data):
 
 
 if "__main__" == __name__:
-    print(decode_mod("file2.lol"))
+    print(decode_mod("file4.lol"))
 
 
     
