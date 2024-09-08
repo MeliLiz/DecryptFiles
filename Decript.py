2573f601f748e4337d91be1b1644a19ea74c89de
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-09-08 09:14:19 -0600

    main

--- Decript.py
+++ Decript.py
@@ -107,15 +107,15 @@ def decode_mod(file_path, decrypted_name):
 
 
 if "__main__" == __name__:
-    """if len(sys.argv) < 3:
+    if len(sys.argv) < 3:
         print("Usage: python3 Main.py <input_file> <output_file>")
         sys.exit(1)
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
-        print(decode_mod(input_file, output_file))"""
+        print(decode_mod(input_file, output_file))
     
-    print(decode_mod("file4.lol", "File4_decoded"))
+    #print(decode_mod("file4.lol", "File4_decoded"))
 
 
     
